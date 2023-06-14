151e0202ee85656c51d8830b79ce7911fda5b06b
natasha <nat.parisella@gmail.com>
2023-06-14 13:57:19 +0200

    dallE route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import dalleRoute from "./routes/dalleRoute.js";
 
 dotenv.config();
 
@@ -9,8 +10,12 @@ const PORT = 8080;
 
 app.use(cors());
 
+app.use(express.json({ limig: "50mb" }));
+
+app.use("/api/v1/dalle", dalleRoute);
+
 app.get("/", (req, res) => {
 	res.status(200).json({ message: "Hello, From Dall-E!" });
 });
 
-app.listen(PORT, () => console.log("Connected to port 8080"));
+app.listen(PORT, () => console.log(`Connected to port ${PORT}`));
